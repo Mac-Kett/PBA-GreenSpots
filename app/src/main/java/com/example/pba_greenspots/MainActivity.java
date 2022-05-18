1216efe74710c70b4b9f2fdb816e3a07b4bbb78d
AdriBoro <adrianmborowicz@gmail.com>
2022-05-18 00:49:56 -0300

    refactor android to androidx

--- app/src/main/java/com/example/pba_greenspots/MainActivity.java
+++ app/src/main/java/com/example/pba_greenspots/MainActivity.java
@@ -1,6 +1,6 @@
 package com.example.pba_greenspots;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
