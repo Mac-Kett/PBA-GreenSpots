6dfc16b548e5c5e470fb764879d86f1935eec73d
Ivan Gianetti <ivangianetti@gmail.com>
2022-05-18 14:31:25 -0300

    initial commit branch register

---
+++ app/src/main/java/com/example/pba_greenspots/registro.java
@@ -0,0 +1,36 @@
+package com.example.pba_greenspots;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class registro extends Fragment {
+
+    private RegistroViewModel mViewModel;
+
+    public static registro newInstance() {
+        return new registro();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_registro, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(RegistroViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
