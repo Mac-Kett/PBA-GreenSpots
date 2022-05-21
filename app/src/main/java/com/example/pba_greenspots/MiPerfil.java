3a6240feb64c93deaca8ecc019037a749ba6c566
Ivan Gianetti <ivangianetti@gmail.com>
2022-05-21 12:49:33 -0300

    layout mi perfil

---
+++ app/src/main/java/com/example/pba_greenspots/MiPerfil.java
@@ -0,0 +1,36 @@
+package com.example.pba_greenspots;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MiPerfil extends Fragment {
+
+    private MiPerfilViewModel mViewModel;
+
+    public static MiPerfil newInstance() {
+        return new MiPerfil();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_mi_perfil, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(MiPerfilViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
