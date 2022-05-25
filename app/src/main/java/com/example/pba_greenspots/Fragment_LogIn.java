28e0a6fc2d68bfd93e1725f9eab3d8a3dd48a929
AdriBoro <adrianmborowicz@gmail.com>
2022-05-25 13:52:25 -0300

    IntegracionConBDyOtrosCambiosParaPoderTestear

--- app/src/main/java/com/example/pba_greenspots/Fragment_LogIn.java
+++ app/src/main/java/com/example/pba_greenspots/Fragment_LogIn.java
@@ -1,62 +1,98 @@
 package com.example.pba_greenspots;
 
+import android.content.SharedPreferences;
+import android.nfc.Tag;
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Fragment_LogIn#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
 public class Fragment_LogIn extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private Button btn1;
+    private EditText etMail, etPass;
 
     public Fragment_LogIn() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment LogIn.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Fragment_LogIn newInstance(String param1, String param2) {
-        Fragment_LogIn fragment = new Fragment_LogIn();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_log_in, container, false);
+
+        View v = inflater.inflate(R.layout.fragment_log_in, container, false);
+        etMail = (EditText) v.findViewById(R.id.etMail);
+        etPass = (EditText) v.findViewById(R.id.etPass);
+        btn1 = (Button) v.findViewById(R.id.btn1);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                login(view);
+            }
+        });
+
+        return v;
     }
+
+    private void login(View view){
+        CollectionReference usersCol = FirebaseFirestore.getInstance().collection("User");
+
+        String mail=etMail.getText().toString();
+        String pass =etPass.getText().toString();
+
+        if (!mail.equals("")&&!pass.equals("")){
+            usersCol.whereEqualTo("Mail", mail)
+                    .limit(1)
+                    .whereEqualTo("Password", pass)
+                    .get()
+                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                            if (task.isSuccessful()) {
+                                if (task.getResult().getDocuments().size()!=0) {
+                                    for (QueryDocumentSnapshot document : task.getResult()) {
+                                        Object obj = document.getData();
+
+                                        Log.d("Fragment_LogIn", document.getId() + " => " + document.getData());
+                                        Toast.makeText(getContext(), "Bienvenido!", Toast.LENGTH_LONG).show();
+                                    }
+                                } else{
+                                    Log.d("Fragment_LogIn", "No hay documento coincidente");
+                                    Toast.makeText(getContext(), "No se encontro!", Toast.LENGTH_LONG).show();
+                                }
+
+                            } else {
+                                Log.d("Fragment_LogIn", "Error getting documents: ", task.getException());
+                            }
+                        }
+                    });
+        }else{
+            Toast.makeText(this.getContext(), "Ingrese ambos datos, por favor!", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void onStart() {
+        super.onStart();
+    }
+
+
 }
--- app/src/main/java/com/example/pba_greenspots/MainActivity.java
+++ app/src/main/java/com/example/pba_greenspots/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.pba_greenspots;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
