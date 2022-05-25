0e1ff07887615a398e4b073ed8a45fcc47e55297
Mac-Kett <maca91@hotmail.com>
2022-05-25 10:44:39 -0300

    Conexión a Data Base

--- app/src/main/java/com/example/pba_greenspots/MainActivity.java
+++ app/src/main/java/com/example/pba_greenspots/MainActivity.java
@@ -3,11 +3,14 @@ package com.example.pba_greenspots;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.fragment_log_in);
     }
 }
