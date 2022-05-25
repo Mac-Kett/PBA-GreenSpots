5ffbfb983f181ac60bbf13d0a3b59f96668f1877
Mac-Kett <maca91@hotmail.com>
2022-05-25 13:45:24 -0300

    Revert "Conexión a Data Base"
    This reverts commit 0e1ff07887615a398e4b073ed8a45fcc47e55297.

--- app/src/main/java/com/example/pba_greenspots/Fragment_LogIn.java
+++ app/src/main/java/com/example/pba_greenspots/Fragment_LogIn.java
@@ -2,42 +2,44 @@ package com.example.pba_greenspots;
 
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
-
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
-
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link Fragment_LogIn#newInstance} factory method to
  * create an instance of this fragment.
  */
-
 public class Fragment_LogIn extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private EditText etMail;
-    private EditText etPass;
-    private Button btn1;
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
 
     public Fragment_LogIn() {
         // Required empty public constructor
     }
 
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment LogIn.
+     */
     // TODO: Rename and change types and number of parameters
-    public static Fragment_LogIn newInstance(EditText etMail, EditText etPass, Button btn1) {
+    public static Fragment_LogIn newInstance(String param1, String param2) {
         Fragment_LogIn fragment = new Fragment_LogIn();
         Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -45,48 +47,16 @@ public class Fragment_LogIn extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
     }
-
-    FirebaseAuth auth;
-    //FirebaseDatabase database;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-        View view = inflater.inflate(R.layout.fragment_log_in, container, false);
-
-        //Inicializo Auth de Firebase
-        auth = FirebaseAuth.getInstance();
-
-        //Inicializo las variables
-        etMail = view.findViewById(R.id.etMail);
-        etPass = view.findViewById(R.id.etPass);
-        btn1 = view.findViewById(R.id.btn1);
-
-        //Pongo el button a la espera
-        btn1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //Los paso a String porque es el tipo de datos que espera la DB
-                String mail = etMail.getText().toString();
-                String pass = etPass.getText().toString();
-
-                //Auth hace su magia
-                auth.signInWithEmailAndPassword(mail, pass).addOnCompleteListener(getActivity(), task -> {
-                    if (task.isSuccessful()) {
-                        Log.d("LogIn", "LogIn Successful");
-                        Toast.makeText(getActivity(), "LogIn Successful", Toast.LENGTH_SHORT).show();
-                    } else {
-                        Log.d("LogIn", "LogIn Failed");
-                        Toast.makeText(getActivity(), "LogIn Failed", Toast.LENGTH_SHORT).show();
-                    }
-                });
-
-                DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Users");
-                reference.child(mail).child("Password").setValue(pass);
-            }
-        });
-        return view;
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_log_in, container, false);
     }
 }
--- app/src/main/java/com/example/pba_greenspots/MainActivity.java
+++ app/src/main/java/com/example/pba_greenspots/MainActivity.java
@@ -3,14 +3,11 @@ package com.example.pba_greenspots;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_log_in);
+        setContentView(R.layout.activity_main);
     }
 }
