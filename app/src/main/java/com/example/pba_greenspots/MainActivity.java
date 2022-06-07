1463568a42669acc968db1f6e037e0a332180872
Ivan Gianetti <ivangianetti@gmail.com>
2022-06-07 14:43:20 -0300

    layout final registro

--- app/src/main/java/com/example/pba_greenspots/MainActivity.java
+++ app/src/main/java/com/example/pba_greenspots/MainActivity.java
@@ -1,47 +1,16 @@
 package com.example.pba_greenspots;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentTransaction;
-
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-
-
-import com.example.pba_greenspots.fragments.PerfilUsuarioFragment;
-
 
 
 public class MainActivity extends AppCompatActivity {
-    private Button btnComenzar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnComenzar = (Button) findViewById(R.id.btnComenzar);
-
-        btnComenzar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-
-                //RegisterFragment register = new RegisterFragment();
-
-            PerfilUsuarioFragment perfil = new PerfilUsuarioFragment();
-             FragmentTransaction fragmentTransaction =getSupportFragmentManager().beginTransaction();
-             fragmentTransaction.replace(R.id.fragment_container, perfil);
-             fragmentTransaction.commit();
-              // FragmentTransaction  fragmentTransaction = getSupportFragmentManager().beginTransaction();
-              // fragmentTransaction.replace(R.id.fragment_container, perfil);
-              // fragmentTransaction.commit();
-
-
-            }
-        });
     }
-
-
-
 
 }
